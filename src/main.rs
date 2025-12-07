9c5f9c234b1b9c4a770b1a41c759e0b1e60016b0
PORTALSURFER <portalsurfer@gmail.com>
2025-12-07 15:56:06 +0100

    fix: start UI maximized without conflicting initial size

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use eframe::egui;
 use egui::viewport::IconData;
 use sempal::audio::AudioPlayer;
-use sempal::egui_app::ui::{DEFAULT_VIEWPORT_SIZE, EguiApp, MIN_VIEWPORT_SIZE};
+use sempal::egui_app::ui::{EguiApp, MIN_VIEWPORT_SIZE};
 use sempal::waveform::WaveformRenderer;
 
 /// Launch the egui UI.
@@ -11,7 +11,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let player = None::<std::rc::Rc<std::cell::RefCell<AudioPlayer>>>;
 
     let mut viewport = egui::ViewportBuilder::default()
-        .with_inner_size(DEFAULT_VIEWPORT_SIZE)
         .with_min_inner_size(MIN_VIEWPORT_SIZE)
         .with_maximized(true);
     if let Some(icon) = load_app_icon() {
